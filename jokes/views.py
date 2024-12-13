785ac4eb52bbcba3c6567fac6f1c1d952b4c636c
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-12-13 14:16:19 +0100

    opdrachten 13,14,15

--- jokes/views.py
+++ jokes/views.py
@@ -1,12 +1,25 @@
 # from django.shortcuts import render
 
 
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
 from .models import Joke
-4.
+from django.urls import reverse_lazy
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
 class JokeDetailView(DetailView):
     model = Joke
 
 class JokeListView(ListView):
     model = Joke
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
