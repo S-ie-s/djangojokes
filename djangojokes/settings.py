0469be7554a05b1b8abc7751e5684fce3e5507d5
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-12-13 10:25:32 +0100

    started exercise 9

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local apps
+    'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
 ]
 
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    ]
