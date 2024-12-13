a02a55842d7eafa6c74ce37058ee9ffac557d248
Sies Frauenfelder <siesfrauenfelder@gmail.com>
2024-12-13 13:49:38 +0100

    absolute url

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,12 @@
 # from django.shortcuts import render
 
-from django.views.generic import ListView
+
+from django.views.generic import DetailView, ListView
 
 from .models import Joke
+4.
+class JokeDetailView(DetailView):
+    model = Joke
 
 class JokeListView(ListView):
     model = Joke
